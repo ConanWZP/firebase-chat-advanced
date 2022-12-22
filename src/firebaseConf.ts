3413bbcd9bbc004db869907004e5af2b92f36706
ConanWZP <yaroslav2281337@mail.ru>
2022-12-22 21:33:23 +0300

    Add env

--- src/firebaseConf.ts
+++ src/firebaseConf.ts
@@ -7,7 +7,7 @@ import { getStorage } from 'firebase/storage'
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
-const firebaseConfig = {
+/*const firebaseConfig = {
     apiKey: "AIzaSyBinXRnBEFn7mecNrqgVbqMmKjuBnnb_nE",
     authDomain: "messeger-1b5c3.firebaseapp.com",
     databaseURL: "http://messeger-1b5c3.firebaseapp.com",
@@ -15,6 +15,16 @@ const firebaseConfig = {
     storageBucket: "messeger-1b5c3.appspot.com",
     messagingSenderId: "281400176411",
     appId: "1:281400176411:web:397f5d2979945478462dbe"
+};*/
+
+const firebaseConfig = {
+    apiKey: process.env.REACT_APP_API_KEY,
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    databaseURL: process.env.REACT_APP_DATABASE_URL,
+    projectId: process.env.REACT_APP_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_APP_ID
 };
 
 // Initialize Firebase
